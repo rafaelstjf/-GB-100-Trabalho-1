f4a9cd2bfcbebd447272ca29e7d6ccc5963ee26d
Rafael Terra <rafaelstjf@hotmail.com>
2022-09-28 16:05:29 -0300

    fixes

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <random>
 #include <assert.h>
 #include <ctime>
+
 #define MAX_VALUE 1000L
 #define TEST_SEED 123456
 std::mt19937 mt(TEST_SEED); //Generator as a global variable
@@ -27,15 +28,19 @@ using namespace std;
  * @param m: Número de linhas do vetor
  * @return int*: Retorna o vetor resultante da multiplicação
  */
-long int* multiply(long int** matrix, long int* vector, long int n, long int m){
-    long int* c = new long int[n];
-    //malloc(sizeof(int)*m);
-    for(long int i = 0; i < m; i++)
-        c[i] = 0;
-    for(long int i  = 0; i < n; i++){
-        for(long int j = 0; j < m; j++){
+long* multiply(long** matrix, long* vector, long n, long m){
+    long* c = new long[n];
+    if(c){
+        for(long i = 0; i < n; i++)
+        c[i] = 0L;
+    }
+    else{
+        exit(-1);
+    }
+    for(long i  = 0; i < n; i++){
+        for(long j = 0; j < m; j++){
             c[i] += matrix[i][j]*vector[j];
-            cout << "c[" << i << "]: = " << "M[" << i << "][" << j << "]*b[" << j << "]" << endl;
+            //cout << "c[" << i << "]: = " << "M[" << i << "][" << j << "]*b[" << j << "]" << endl;
             //cout << c[i] << " = " << matrix[i][j] << " * " << vector[j] << endl;
         }
     }
@@ -49,19 +54,19 @@ long int* multiply(long int** matrix, long int* vector, long int n, long int m){
  * @param n: Número de linhas da matriz
  * @param m: Número de linhas do vetor
  * @param tile_size: Tamanho do tile
- * @return int*: Retorna o vetor resultante da multiplicação
+ * @return long*: Retorna o vetor resultante da multiplicação
  */
-long int* multiplyCacheBlocking(long int** matrix, long int* vector, long int n, long int m, int tile_size){
-    assert(m % tile_size == 0);
-    long int* c = new long int[n];
-    for(long int i =0; i < m; i++){
-        c[i] = 0;
+long* multiplyCacheBlocking(long** matrix, long* vector, long n, long m, int tile_size){
+    assert(m % (long)tile_size == 0);
+    long* c = new long[n];
+    for(long i =0; i < n; i++){
+        c[i] = 0L;
     }
-    for(long int jj = 0; jj < m; jj+=tile_size){
-        for(long int i  = 0; i < n; i++){
-            for(long j = jj; j < jj + tile_size; j++){
+    for(long jj = 0; jj < m; jj+=(long)tile_size){
+        for(long i  = 0; i < n; i++){
+            for(long j = jj; j < jj + (long)tile_size; j++){
                 c[i] += matrix[i][j]*vector[j];
-                cout << "c[" << i << "]: = " << "M[" << i << "][" << j << "]*b[" << j << "]" << endl;
+                //cout << "c[" << i << "]: = " << "M[" << i << "][" << j << "]*b[" << j << "]" << endl;
                 //cout << c[i] << " = " << matrix[i][j] << " * " << vector[j] << endl;
             }
         }
@@ -75,13 +80,12 @@ long int* multiplyCacheBlocking(long int** matrix, long int* vector, long int n,
  * @param m: Número de colunas
  * @return long**: Retorna a matriz criada
  */
-long int** create_matrix(int n, int m){
-    uniform_int_distribution<long int> uniform{0L,MAX_VALUE};
-    long int** matrix  = new long int*[n];
-    //(int*)malloc(sizeof(int*)*n);
-    for(long int i = 0; i < n; i++){
-        matrix[i] = new long int[m];
-        for(long int j = 0; j < m; j++){
+long** create_matrix(long n, long m){
+    uniform_int_distribution<long> uniform{0L,MAX_VALUE};
+    long** matrix  = new long*[n];
+    for(long i = 0; i < n; i++){
+        matrix[i] = new long[m];
+        for(long j = 0; j < m; j++){
             matrix[i][j] = uniform(mt);
         }
     }
@@ -94,10 +98,10 @@ long int** create_matrix(int n, int m){
  * @param m: Número de linhas do vetor
  * @return long*: Retorna o vetor criado 
  */
-long int* create_vector(long int m){
-    uniform_int_distribution<long int> uniform{0L,MAX_VALUE};
-    long int* vector = new long int[m];
-    for(long int i = 0; i < m; i++){
+long* create_vector(long m){
+    uniform_int_distribution<long> uniform{0L,MAX_VALUE};
+    long* vector = new long[m];
+    for(long i = 0; i < m; i++){
         vector[i] = uniform(mt);
     }
     return vector;
@@ -112,29 +116,29 @@ long int* create_vector(long int m){
  * @param n: Número de linhas da matriz
  * @param m: Número de colunas da matriz 
  */
-void printElements(long int** matrix, long int* vector, long int *c, long int n, long int m){
+void printElements(long** matrix, long* vector, long *c, long n, long m){
     cout << "Matrix: " << endl;
-    for(long int i = 0; i < n; i++){
-        for(long int j = 0; j < m; j++){
+    for(long i = 0; i < n; i++){
+        for(long j = 0; j < m; j++){
             cout << matrix[i][j] << "\t";
         }
         cout << endl;
     }
     cout << "----------------------------------" << endl;
     cout << "Vector: " << endl;
-    for(long int i = 0; i < m; i++){
+    for(long i = 0; i < m; i++){
         cout << vector[i] << endl;
     }
     cout << "----------------------------------" << endl;
     cout << "Resulting vector: " << endl;
-    for(long int i = 0; i < n; i++){
+    for(long i = 0; i < n; i++){
         cout << c[i] << endl;
     }
     cout << "----------------------------------" << endl;
 }
 
 int main(int argc, char* argv[]){
-    long int m = 2000, n = 3000;
+    long m = 2000L, n = 2000L;
     int use_cache = 0, tile_size=4;
     if (argc > 4){
         n = atol(argv[1]);
@@ -146,27 +150,19 @@ int main(int argc, char* argv[]){
         cerr << "Arguments needed (in order): \"number of lines\" \"number of columns\" \"0 for normal multiplication or 1 for using cache blocking\" \"tile size\"" << endl;
         exit(-1);
     }
-    long int* vector  = create_vector(m);
-    long int** matrix = create_matrix(n, m);
-    long int* c = nullptr;
+    long* vector  = create_vector(m);
+    long** matrix = create_matrix(n, m);
+    long* c = nullptr;
     if (use_cache)
         c = multiplyCacheBlocking(matrix, vector, n, m, tile_size);
     else
         c = multiply(matrix, vector, n, m);
-
     printElements(matrix, vector, c, n, m);
-    /*
-    clock_t t;
-    t = clock();
-    t = clock() - t;
-    float time_seconds = ((float)t)/CLOCKS_PER_SEC;
-    cout << "The execution took " << time_seconds << " seconds." << endl;
-    */
-    delete vector;
-    delete c;
-    for(long int i = 0; i < n; i++){
-        delete matrix[i];
+    delete[] vector;
+    delete[] c;
+    for(long i = 0; i < n; i++){
+        delete[] matrix[i];
     }
-    delete matrix;
+    delete[] matrix;
     return 0;
 }
