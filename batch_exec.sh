9d73b93da4c47e1c0bda2cf1a3e970d7d9e18746
Rafael Terra <rafaelstjf@hotmail.com>
2022-10-02 16:18:21 -0300

    Measuring time using \time

--- batch_exec.sh
+++ batch_exec.sh
@@ -7,11 +7,9 @@ use_cache=1
 for (( n = 64; n <= 128; n=2*n ));do
     for (( m = 16384; m <= 1048576; m=2*m ));do
         for (( t = 128; t <= 16384; t=2*t));do
-            START=$(date +%s.%N)
-            ./main $n $m $use_cache $t
-            END=$(date +%s.%N)
-            printf -v DELTA "%g" $(bc <<<"$END - $START")
-            buffer+="${n},${m},${DELTA},$t"
+            time_exec=$(\time -f '%e' ./main $n $m $use_cache $t 2>&1)
+            echo "Time: $time_exec"
+            buffer+="${n},${m},${time_exec},$t"
             buffer+=$'\n'
         done
     done
